5e741793bbb48aa9ca286431c86d05ec58601518
hqy <1012927920@qq.com>
2022-05-24 18:49:34 +0800

    update readme

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # 文件上传DEMO
 
 本DEMO实现了分片上传，并限制了并发数。
+
 使用Web-Worker计算文件hash值来实现秒传。
+
 实现了服务端的断点续传。
+
+## 使用
+```shell
+cd ./front/
+yarn serve
+
+cd ./back/
+node ./app.js
+```
+
+**注：分片默认为100byte，请合理控制文件大小。**
--- back/fs.js
+++
@@ -1,8 +0,0 @@
-const path = require('path')
-const fs = require('fs')
-
-// console.log(fs.readdirSync(path.join(__dirname, '/upload')))
-
-fs.rmSync(path.join(__dirname, '/upload'), {recursive: true})
-
-// fs.exists
